3d30e2c9608057cffdcfc7f5e02282fe6bdd9ab8
yeghro <yeghro@proton.me>
2024-06-08 00:52:20 -0700

    made getalby initiated only after the login is clicked

--- src/nostrService.js
+++ src/nostrService.js
@@ -1,23 +1,6 @@
 import { NDKNip07Signer } from "@nostr-dev-kit/ndk";
 
-// Attempt to instantiate the NDKNip07Signer
-let nip07Signer = null;
-let activeUser = null;
-
-(async () => {
-  try {
-    nip07Signer = new NDKNip07Signer(5000);
-    activeUser = await nip07Signer.blockUntilReady();
-    // console.log("Signer is ready and user is:", activeUser);
-  } catch (error) {
-    console.warn("NDKNip07Signer not available or user denied access:", error);
-    nip07Signer = null;
-    activeUser = null;
-  }
-})();
-
-export { nip07Signer, activeUser };
-
+// Initialize relay URLs
 export let relayUrls = [
   "wss://purplepag.es",
   "wss://relay.nostr.band",
@@ -30,22 +13,36 @@ export let relayUrls = [
 
 export let relays = {};
 
-export function connectToRelays() {
+let nip07Signer = null;
+let activeUser = null;
+
+export async function connectToRelays() {
+  try {
+    nip07Signer = new NDKNip07Signer(5000);
+    activeUser = await nip07Signer.blockUntilReady();
+    console.log("Signer is ready and user is:", activeUser);
+  } catch (error) {
+    console.warn("NDKNip07Signer not available or user denied access:", error);
+    nip07Signer = null;
+    activeUser = null;
+    return; // Exit the function if signing fails
+  }
+
   relayUrls.forEach((url) => {
     const ws = new WebSocket(url);
 
     ws.onopen = () => {
-      // console.log(`Connected to relay: ${url}`);
+      console.log(`Connected to relay: ${url}`);
       relays[url] = ws;
     };
 
     ws.onmessage = (event) => {
-      // console.log(`Message from ${url}:`, event.data);
+      console.log(`Message from ${url}:`, event.data);
       // Handle incoming messages
     };
 
     ws.onclose = () => {
-      // console.log(`Disconnected from relay: ${url}`);
+      console.log(`Disconnected from relay: ${url}`);
       delete relays[url];
     };
 
@@ -53,5 +50,8 @@ export function connectToRelays() {
       console.error(`Error from relay ${url}:`, error);
     };
   });
-  // console.log("Established Relay Instance:", relays);
+
+  console.log("Established Relay Instance:", relays);
 }
+
+export { nip07Signer, activeUser };
